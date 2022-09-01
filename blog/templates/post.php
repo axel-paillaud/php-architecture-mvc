afbb3977ed561f09bcac52076d67eeab0c940d16
Shaun-maker <a.paillaud75@gmail.com>
2022-09-01 12:21:23 +0200

    start to add comment function

--- blog/templates/post.php
+++ blog/templates/post.php
@@ -1,36 +1,42 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le blog de l'AVBN: xxx</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-<body>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php $title = "Le blog de l'AVBN"; ?>
 
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($post['title']) ?>
-            <em>le <?= $post['french_comment_date'] ?></em>
-        </h3>
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
 
-        <p>
-            <?= nl2br(htmlspecialchars($post['content'])) ?>
-        </p>
-    </div>
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($post['title']) ?>
+        <em>le <?= $post['french_comment_date'] ?></em>
+    </h3>
 
-    <h2>Commentaires</h2>
-
-    <?php foreach ($comments as $comment) : ?>
     <p>
-        <strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['french_comment_date'] ?>
+        <?= nl2br(htmlspecialchars($post['content'])) ?>
     </p>
-    <p>
-        <?= nl2br(htmlspecialchars($comment['comment'])) ?>
-    </p>
-    <?php endforeach; ?>
-</body>
-</html>
+</div>
+
+<h2>Commentaires</h2>
+
+<form action="index.php?send" method="post">
+    <label>Votre commentaire</label>
+    <br/>
+    <textarea autocomplete="off" name="comment" placeholder="Veuillez svp rester respectueux envers les autres"></textarea>
+    <br/><br/>
+    <label>Auteur</label>
+    <br/>
+    <input type="text" autocomplete="off" name="author" placeholder="Nom et prénom">
+    <br/><br/>
+    <button type="submit">Envoyer</button>
+</form>
+
+<?php foreach ($comments as $comment) : ?>
+<p>
+    <strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['french_comment_date'] ?>
+</p>
+<p>
+    <?= nl2br(htmlspecialchars($comment['comment'])) ?>
+</p>
+<?php endforeach; ?>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('layout.php'); ?>
